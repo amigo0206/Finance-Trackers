9d86650c03c97072c3ce9a69fd575663a6adb622
Amgad Amer <thedeveloperguru@gmail.com>
2016-03-04 02:56:46 +0000

    Edited index page

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'pages#home'
-  get 'about', to: 'pages#about'
+  root 'welcome#index'
+  get 'about', to: 'welcome#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
